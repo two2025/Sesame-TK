0f45b820b5d35cfe61ec20bc0f8ad74e0e5011e2
two2024 <216294726+two2025@users.noreply.github.com>
2025-08-12 13:05:27 +0800

    Update DeviceInfo.kt

--- app/src/main/java/fansirsqi/xposed/sesame/util/DeviceInfo.kt
+++ app/src/main/java/fansirsqi/xposed/sesame/util/DeviceInfo.kt
@@ -90,9 +90,10 @@ fun DeviceInfoCard(info: Map<String, String>) {
             Text(
                     text = "《 F ❤ C 》",
                     fontSize = 12.sp,
-                    color = Color.Red,
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
+                    color = Color.Red
+                // ,
+                //     modifier = Modifier.fillMaxWidth(),
+                //     textAlign = TextAlign.Center
             )
         }
     }
